c315ddafd9d17e78bf97c7762feefce5c4888614
tonga2402 <gbalmaceda@live.com>
2024-10-08 23:22:32 -0300

    ButtonGroup agregado

--- app/clients/[clientId]/page.tsx
+++ app/clients/[clientId]/page.tsx
@@ -1,5 +1,4 @@
-import ClientTab from '@/app/components/ClientTab';
-import SelectMenu from '@/app/components/SelectMenu';
+import ButtonGroup from '@/app/components/ButtonGroup';
 import { ListClientType } from '@/app/types/ListClient.Type';
 import React from 'react'
 
@@ -21,37 +20,40 @@ const page = async ({
     const clientId = searchParams["c"]
     const clientDetail = await getClient({ clientId })
     return (
-        <div className="flex mt-28 justify-center w-full ">
-            <div className="max-w-screen-xl relative overflow-hidden shadow-md rounded-lg">
-                <table className="table-fixed w-full text-left">
-                    <thead
-                        className="uppercase bg-[#6b7280] text-[#e5e7eb]"
-                        style={{ backgroundColor: "#6b7280", color: "#e5e7eb" }}
-                    >
-                        <tr>
-                            <td className="py-3 border text-center  p-4">Cliente</td>
-                            <td className="py-3 border text-center  p-4">Parcelas</td>
-                        </tr>
-                    </thead>
-                    <tbody
-                        className="bg-[#FFFFFF] text-[#6b7280]"
-                        style={{ backgroundColor: "#FFFFFF", color: "#6b7280" }}
-                    >
-                        <tr className="py-5">
-                            <td className="py-5 border text-center p-4">{clientDetail.name}</td>
-                            <select className="w-full py-5 text-center p-4 bg-[#FFFFFF] text-[#6b7280]" name="" id="">
-                                <option value="">uno</option>
-                                <option value="">dos</option>
-                                <option value="">tres</option>
-                                <option value="">cuatro</option>
-                                <option value="">cinco</option>
+        <>
+            <div className="flex flex-col items-center mt-10 justify-center w-full gap-10">
+            <ButtonGroup />
+                <div className="max-w-screen-xl relative overflow-hidden shadow-md rounded-lg">
+                    <table className="table-fixed w-full text-left">
+                        <thead
+                            className="uppercase bg-[#6b7280] text-[#e5e7eb]"
+                            style={{ backgroundColor: "#6b7280", color: "#e5e7eb" }}
+                        >
+                            <tr>
+                                <td className="py-3 border text-center  p-4">Cliente</td>
+                                <td className="py-3 border text-center  p-4">Parcelas</td>
+                            </tr>
+                        </thead>
+                        <tbody
+                            className="bg-[#FFFFFF] text-[#6b7280]"
+                            style={{ backgroundColor: "#FFFFFF", color: "#6b7280" }}
+                        >
+                            <tr className="py-5">
+                                <td className="py-5 border text-center p-4">{clientDetail.name}</td>
+                                <select className="w-full py-5 text-center p-4 bg-[#FFFFFF] text-[#6b7280]" name="" id="">
+                                    <option value="">uno</option>
+                                    <option value="">dos</option>
+                                    <option value="">tres</option>
+                                    <option value="">cuatro</option>
+                                    <option value="">cinco</option>
 
-                            </select>
-                        </tr>
-                    </tbody>
-                </table>
+                                </select>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
---
+++ app/components/ButtonGroup.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const ButtonGroup = () => {
+    return (
+
+
+        <div className="inline-flex rounded-md shadow-sm" role="group">
+            <button type="button" className="px-4 py-2 text-sm font-medium text-[#e5e7eb] bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100  focus:z-10  dark:bg-[#6b7280] dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-[#0f6e75] dark:focus:bg-[#0f6e75] dark:focus:text-white">
+                Parcelas
+            </button>
+            <button type="button" className="px-4 py-2 text-sm font-medium text-[#e5e7eb] bg-white border border-gray-200 hover:bg-gray-100  focus:z-10  dark:bg-[#6b7280] dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-[#0f6e75] dark:focus:bg-[#0f6e75] dark:focus:text-white">
+                Campanas
+            </button>  <button type="button" className="px-4 py-2 text-sm font-medium text-[#e5e7eb] bg-white border border-gray-200 hover:bg-gray-100  focus:z-10  dark:bg-[#6b7280] dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-[#0f6e75] dark:focus:bg-[#0f6e75] dark:focus:text-white">
+                Aplicacion
+            </button>  <button type="button" className="px-4 py-2 text-sm font-medium text-[#e5e7eb] bg-white border border-gray-200 hover:bg-gray-100  focus:z-10  dark:bg-[#6b7280] dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-[#0f6e75] dark:focus:bg-[#0f6e75] dark:focus:text-white">
+                Productos
+            </button>  <button type="button" className="px-4 py-2 text-sm font-medium text-[#e5e7eb] bg-white border border-gray-200 hover:bg-gray-100  focus:z-10  dark:bg-[#6b7280] dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-[#0f6e75] dark:focus:bg-[#0f6e75] dark:focus:text-white">
+                Fertilizantes
+            </button>
+            <button type="button" className="px-4 py-2 text-sm font-medium text-[#e5e7eb] bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 focus:z-10  dark:bg-[#6b7280] dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-[#0f6e75] dark:focus:bg-[#0f6e75] dark:focus:text-white">
+                Trabajos
+            </button>
+        </div>
+
+    )
+}
+
+export default ButtonGroup
